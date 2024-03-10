e43fbae33bc461456e0b1331650b4b9be0f7ca1d
vyasp <vyasparth451@gmail.com>
2024-03-10 14:25:11 +0530

    Update Password For USer

--- lib/ui/SettingScreen/updatepassword/update_password_controller.dart
+++ lib/ui/SettingScreen/updatepassword/update_password_controller.dart
@@ -1,5 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:home_hub_services/ModelClasses/servicesProvider.dart';
+import 'package:home_hub_services/getstorage/StorageClass.dart';
+import 'package:home_hub_services/utils/app_routes.dart';
 
-class UpdatePasswordController extends GetxController{
+class UpdatePasswordController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController currentPasswordController =
+  TextEditingController();
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+  TextEditingController();
 
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    confirmPasswordController.dispose();
+    newPasswordController.dispose();
+    currentPasswordController.dispose();
+  }
+  StorageService storageService = StorageService();
+  User? UserID = FirebaseAuth.instance.currentUser;
+
+  RxBool isLoading = false.obs;
+
+  void handelPasswordError(ServicesData myDataObject, BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      isLoading.value = true;
+      Future.delayed(Duration(seconds: 2), () async {
+        isLoading.value = false;
+        // service_providers
+        String uid = storageService.getUserid();
+        final DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('service_providers').doc(uid);
+        UserCredential userCredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+            email: myDataObject.email, password: myDataObject.password!);
+        if (userCredential != null) {
+          await userCredential.user!
+              .updatePassword(confirmPasswordController.text.toString().trim());
+          myDataObject.password = confirmPasswordController.text.toString();
+          await documentReference.update(myDataObject.tomap());
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Password changed successfully!'),
+          ));
+          storageService.loginStatusCheck(false);
+          Get.offAllNamed(Routes.loginScreen);
+          print("Password Changes");
+        } else {
+          print("User Not Found");
+        }
+      });
+    }
+  }
 }
